96e968643096b335738985d03c5a5ad146a109df
Alexander Jimenez <stevenhernandezj@gmail.com>
2023-09-26 19:53:00 -0500

    Update 03-tipos-de-datos.js

--- hola-mundo/files/03-tipos-de-datos.js
+++ hola-mundo/files/03-tipos-de-datos.js
@@ -1,5 +1,7 @@
+//compatibilidad de JS escenarios como librerias o integraciones con chromium, esto permite habilitar y traspilar lo que hacer que valide que todo este bien.
 'use strict'
 
+// usar let const var
 var numero1 = 7;
 var numero_falso = "33";
 var bool = true;
